51981ced252cb3c0c178579f49e2e5f8e0757a93
Tracy Redman <tracyredman@Tracys-MacBook-Air.local>
2022-03-27 20:35:51 -0400

    2ndPush

--- PairProgamming.UI/Program.cs
+++ PairProgamming.UI/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// char board [3][3]
+// const char PLAYER = 'X';
+// const char COMP = '0';
+
+// void resetBoard();
+// void printBoard();
+// int checkFreeSpaces;
+// void playerMove();
+// void computerMove();
+// char checkWinner();
+// void printWinner(char);
+
+// int main ()
+// {
+//     char winner =' ';
+// }
+
+// void resetBoard();
+// void printBoard();
+// int  checkFreeSpaces;
+// void playerMove();
+// void computerMove();
+// char checkWinner();
+// void printWinner(char);
